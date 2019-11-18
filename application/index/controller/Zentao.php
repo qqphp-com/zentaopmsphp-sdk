8ad8d72afeebc9622fdac00f8862191b15b3bcfd
leiyong <1549684884@qq.com>
2019-11-19 00:00:34 +0800

    1119

--- application/index/controller/Zentao.php
+++ application/index/controller/Zentao.php
@@ -7,15 +7,23 @@ namespace app\index\controller;
 class Zentao
 {
     //禅道部署域名
-    const ztUrl = 'http://www.testzentao.com/index.php';
+    const ztUrl = 'http://zentao.qqphp.com/';//http://www.testzentao.com/index.php|http://zentao.qqphp.com/
     //禅道登录账户
     const ztAccount = 'admin';
     //禅道登录密码
     const ztPassword = 'asd401733012';
+    //参数请求方式[GET/PATH_INFO]
+    const requestType = 'PATH_INFO';//PATH_INFO|GET
     //身份认证[sessionName + sessionID]
     public $sessionAuth = '';
     //接口请求参数
-    public $params = [];
+    public $params = array();
+    //返回结果
+    public $returnResult = array(
+        'status' => 0,
+        'msg'    => '操作失败',
+        'result' => array()
+    );
 
     /**
      * 获取登录sessionId
@@ -25,8 +33,7 @@ class Zentao
     {
         $this->params      = [
             'm' => 'api',
-            'f' => 'getSessionID',
-            't' => 'json'
+            'f' => 'getSessionID'
         ];
         $result            = $this->getUrl(self::ztUrl);
         $resultData        = json_decode($result);
@@ -35,65 +42,129 @@ class Zentao
         $this->login();
     }
 
+    /**
+     * 用户登录验证
+     * @return bool|string
+     */
     public function login()
     {
-        $curl         = new HttpCurl();
         $this->params = [
-            'm'        => 'user',
-            'f'        => 'login',
             'account'  => self::ztAccount,
-            'password' => self::ztPassword,
-            't'        => 'json'
+            'password' => self::ztPassword
         ];
-        $result       = $this->getUrl(self::ztUrl);
+        if (self::requestType == 'GET') {
+            $this->params = array_merge($this->params, [
+                'm' => 'user',
+                'f' => 'login',
+            ]);
+            $result       = $this->getUrl(self::ztUrl);
+        } elseif (self::requestType == 'PATH_INFO') {
+            $result = $this->postUrl(self::ztUrl . '/user-login.json');
+        }
         return $result;
     }
 
     /**
      * 组织-获取部门列表
-     * @return bool|mixed
+     * @param array $optionalParams
+     * @return false|string
      */
-    public function deptBrowse()
+    public function deptBrowse($optionalParams = ['deptID' => 1])
     {
-        $curl         = new HttpCurl();
         $this->params = [
-            'm'      => 'dept',
-            'f'      => 'browse',
-            't'      => 'json',
-            'deptID' => 1
+            'm' => 'dept',
+            'f' => 'browse'
         ];
+        $this->params = array_merge($this->params, $optionalParams);
         $result       = $this->getUrl(self::ztUrl);
-        return $result;
+        $resultData   = json_decode($result);
+        $returnResult = $this->returnResult;
+        if (strcmp($resultData->status, 'success') === 0) {
+            $sessionData = json_decode($resultData->data);
+            if (!empty($sessionData->tree)) {
+                $returnResult = array(
+                    'status' => 1,
+                    'msg'    => '操作成功',
+                    'result' => array(
+                        'title'       => $sessionData->title,
+                        'deptID'      => $sessionData->deptID,
+                        'parentDepts' => $sessionData->parentDepts,
+                        'sons'        => $sessionData->sons,
+                        'tree'        => $sessionData->tree,
+                    )
+                );
+
+            }
+        }
+        return json_encode($returnResult, JSON_UNESCAPED_UNICODE);
     }
 
     /**
      * 组织-添加新部门
-     * @return mixed
+     * @param array $optionalParams
+     * @return false|string
      */
-    public function deptManageChild()
+    public function deptManageChild($optionalParams = ['depts[0]' => 'A部门', 'depts[1]' => 'B部门', 'parentDeptID' => '1'])
     {
-        $curl         = new HttpCurl();
-        $this->params = [
-            'depts[]'      => '2ddddd',
-            'parentDeptID' => '5',//上级部门ID
-            't'            => 'json'
-        ];
-        $result       = $this->postUrl(self::ztUrl . '?m=dept&f=manageChild');
-        return $result;
+        $returnResult = $this->returnResult;
+        if (self::requestType == 'GET') {
+            $this->params = [
+                't' => 'json'
+            ];
+            $this->params = array_merge($this->params, $optionalParams);
+            $result       = $this->postUrl(self::ztUrl . '?m=dept&f=manageChild');
+        } elseif (self::requestType == 'PATH_INFO') {
+            $this->params = [];
+            $this->params = array_merge($this->params, $optionalParams);
+            $result       = $this->postUrl(self::ztUrl . '/dept-manageChild.json');
+        }
+        if (strpos($result, 'reload')) {
+            $returnResult = array(
+                'status' => 1,
+                'msg'    => '操作成功',
+                'result' => array()
+            );
+        }
+        return json_encode($returnResult, JSON_UNESCAPED_UNICODE);
     }
 
-    public function getUrl($url)
+    public function uuuuser($optionalParams = [])
+    {
+        $this->params = [
+            'm' => 'group',
+            'f' => 'browse'
+        ];
+        $this->params = array_merge($this->params, $optionalParams);
+        $result       = $this->getUrl(self::ztUrl);
+        $resultData   = json_decode($result);
+        $returnResult = $this->returnResult;
+        dump($result);die();
+        return $returnResult;
+    }
+
+    public function getUrl($url, $dump = 0)
     {
         $ch = curl_init();
         // 设置get参数
-        if (!empty($this->params) && count($this->params)) {
-            if (strpos($url, '?') !== false) {
-                $url .= http_build_query($this->params);
-            } else {
-                $url .= '?' . http_build_query($this->params);
+        if (self::requestType == 'GET') {
+            $this->params = array_merge($this->params, ['t' => 'json']);
+            if (!empty($this->params) && count($this->params)) {
+                if (strpos($url, '?') !== false) {
+                    $url .= http_build_query($this->params);
+                } else {
+                    $url .= '?' . http_build_query($this->params);
+                }
             }
+        } elseif (self::requestType == 'PATH_INFO') {
+            $params = implode('-', $this->params);
+            $url    = $url . '/' . $params . '.json';
+        }
+        if ($dump) {
+            var_dump($url);
+            die();
         }
         curl_setopt($ch, CURLOPT_COOKIE, $this->sessionAuth);
+        curl_setopt($ch, CURLOPT_REFERER, self::ztUrl);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($ch);
@@ -101,12 +172,13 @@ class Zentao
         return $result;
     }
 
-    public function postUrl($url)
+    public function postUrl($url, $dump = 0)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_COOKIE, $this->sessionAuth);
         curl_setopt($ch, CURLOPT_REFERER, self::ztUrl);
         curl_setopt($ch, CURLOPT_URL, $url);
+
         // 设置post参数
         if (!empty($this->params)) {
             if (is_array($this->params)) {
@@ -114,6 +186,10 @@ class Zentao
             } else if (is_string($this->params)) {
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $this->params);
             }
+        }
+        if ($dump) {
+            var_dump($url);
+            die();
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, true);
