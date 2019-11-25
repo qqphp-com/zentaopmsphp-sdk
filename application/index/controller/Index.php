aae8c9377ad0d386c32243fba2c29df7f3251585
leiyong <1549684884@qq.com>
2019-11-25 18:57:31 +0800

    更新描述详情

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -14,13 +14,126 @@ class Index
      */
     public function deptBrowse()
     {
-//        vendor('zentaopms.Zentao');
-//        require_once('../vendor/zentaopms/Zentao.php');
-        include_once('../vendor/zentaopms/Zentao.php');
-        $zt     = new Zentao();
-        $result = $zt->deptBrowse([
+        include_once('../vendor/zentao/zentao.php');
+        $zentao = new \zentao\zentao\zentao();
+        $params = array(
             'deptID' => 1
-        ]);
+        );
+        $result = $zentao->deptBrowse($params);
+        return $result;
+    }
+
+    /**
+     * Desc:使用示例-批量添加部门
+     */
+    public function deptManageChild()
+    {
+        include_once('../vendor/zentao/zentao.php');
+        $zentao = new \zentao\zentao\zentao();
+        $params = array(
+            'parentDeptID' => '1',
+            'depts[0]'     => 'Department A',
+            'depts[1]'     => 'Department B',
+        );
+        $result = $zentao->deptManageChild($params);
+        return $result;
+    }
+
+    /**
+     * Desc:使用示例-获取用户列表
+     */
+    public function companyBrowse()
+    {
+        include_once('../vendor/zentao/zentao.php');
+        $zentao = new \zentao\zentao\zentao();
+        $params = array(
+            'deptID' => 47
+        );
+        $result = $zentao->companyBrowse($params);
+        return $result;
+    }
+
+    /**
+     * Desc:添加用户可选信息
+     */
+    public function userCreateInfo()
+    {
+        include_once('../vendor/zentao/zentao.php');
+        $zentao = new \zentao\zentao\zentao();
+        $result = $zentao->userCreateInfo();
+        return $result;
+    }
+
+    /**
+     * Desc:新增用戶
+     */
+    public function userCreate()
+    {
+        include_once('../vendor/zentao/zentao.php');
+        $zentao = new \zentao\zentao\zentao();
+        $params = array(
+            'dept'      => 47,
+            'account'   => 'Jack8',
+            'password1' => '123456',
+            'password2' => '123456',
+            'realname'  => 'jack8',
+            'join'      => '2019-11-11',
+            'role'      => 'dev',
+            'group'     => 2,
+            'email'     => 'jack2019@gmail.com',
+            'commiter'  => 'http://jack2019.com',
+            'gender'    => 'm'
+        );
+        $result = $zentao->userCreate($params);
+        return $result;
+    }
+
+    /**
+     * Desc:获取产品列表productAll
+     */
+    public function productAll()
+    {
+        include_once('../vendor/zentao/zentao.php');
+        $zentao = new \zentao\zentao\zentao();
+        $result = $zentao->productAll();
+        return $result;
+    }
+
+    /**
+     * Desc:添加产品可选信息
+     * Date:2019/11/25/025
+     */
+    public function productCreateInfo()
+    {
+        include_once('../vendor/zentao/zentao.php');
+        $zentao = new \zentao\zentao\zentao();
+        $result = $zentao->productCreateInfo();
+        return $result;
+    }
+
+    /**
+     * Desc:添加单个产品
+     * Date:2019/11/25/025
+     */
+    public function productCreate()
+    {
+        include_once('../vendor/zentao/zentao.php');
+        $zentao = new \zentao\zentao\zentao();
+        $params = array(
+            'name'         => 'product-100',
+            'code'         => 'p100',
+            'line'         => 0,
+            'PO'           => 'lisi',
+            'QD'           => 'zhangsan',
+            'RD'           => 'wangwu',
+            'type'         => 'normal',
+            'status'       => 'normal',
+            'desc'         => 'product description,product description',
+            'acl'          => 'custom',
+            'whitelist[0]' => '1',
+            'whitelist[1]' => '2',
+        );
+        $result = $zentao->productCreate($params);
         return $result;
     }
 }
